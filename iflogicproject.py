e898582e7c0bfb623367e93cf32878206921edf3
Ebrimasaho1 <saho.ebrima@ymail.com>
2022-06-30 16:56:05 +0000

    Added more business logic that asks user if they want to continue or not

--- iflogicproject.py
+++ iflogicproject.py
@@ -21,47 +21,146 @@ foodChoices = foodLocations.keys()
 lowerCaseFoodChoices = [x.lower() for x in foodChoices]
 print(lowerCaseFoodChoices)
 
-# having user to select their choice based on our suggestions
-#userFoodChoice = input("Please select your choice above and we'll recommend places to eat! ").lower()
 
 # validation to see if user inputs right choice
-while True:
+keepgoing = True;
+
+while (keepgoing):
 
 # having user to select their choice based on our suggestions
-    userFoodChoice = input("Please select your choice above and we'll recommend places to eat! ").lower()
+    userFoodChoice = input("Please select your choice above and we'll recommend places to eat: ").lower()
     
     # Finding 
     if userFoodChoice == lowerCaseFoodChoices[0]:
         print("Recommendation: " + foodLocations["Cheeseburger"])
-        
+
+        # asking if user wants to keep finding restaurant recommendations
+        moreRecommendations = input("Want more recommendations [Y|N]?").lower()
+        if(moreRecommendations == "y"):
+            userFoodChoice = input("Please select your choice above and we'll recommend places to eat: ").lower()
+        elif (moreRecommendations == "n"):
+            keepgoing = False
+
+        else:
+            continue
+    
     if userFoodChoice == lowerCaseFoodChoices[1]:
         print("Recommendation: " + foodLocations["Chicken Wings"])
+
+        # asking if user wants to keep finding restaurant recommendations
+        moreRecommendations = input("Want more recommendations [Y|N]?").lower()
+        if(moreRecommendations == "y"):
+            userFoodChoice = input("Please select your choice above and we'll recommend places to eat: ").lower()
+        elif (moreRecommendations == "n"):
+            keepgoing = False
+
+        else:
+            continue
         
     if userFoodChoice == lowerCaseFoodChoices[2]:
         print("Recommendation: " + foodLocations["Fried Fish"])
-        
+
+        # asking if user wants to keep finding restaurant recommendations
+        moreRecommendations = input("Want more recommendations [Y|N]?").lower()
+        if(moreRecommendations == "y"):
+            userFoodChoice = input("Please select your choice above and we'll recommend places to eat: ").lower()
+        elif (moreRecommendations == "n"):
+            keepgoing = False
+
+        else:
+            continue
+
     if userFoodChoice == lowerCaseFoodChoices[3]:
         print("Recommendation: " + foodLocations["Dumplings"])
+
+        # asking if user wants to keep finding restaurant recommendations
+        moreRecommendations = input("Want more recommendations [Y|N]?").lower()
+        if(moreRecommendations == "y"):
+            userFoodChoice = input("Please select your choice above and we'll recommend places to eat: ").lower()
+        elif (moreRecommendations == "n"):
+            keepgoing = False
+
+        else:
+            continue
         
     if userFoodChoice == lowerCaseFoodChoices[4]:
         print("Recommendation: " + foodLocations["Korean BBQ"])
+
+        # asking if user wants to keep finding restaurant recommendations
+        moreRecommendations = input("Want more recommendations [Y|N]?").lower()
+        if(moreRecommendations == "y"):
+            userFoodChoice = input("Please select your choice above and we'll recommend places to eat: ").lower()
+        elif (moreRecommendations == "n"):
+            keepgoing = False
+
+        else:
+            continue
         
     if userFoodChoice == lowerCaseFoodChoices[5]:
         print("Recommendation: " + foodLocations["Fillet Mignon"])
+
+        # asking if user wants to keep finding restaurant recommendations
+        moreRecommendations = input("Want more recommendations [Y|N]?").lower()
+        if(moreRecommendations == "y"):
+            userFoodChoice = input("Please select your choice above and we'll recommend places to eat: ").lower()
+        elif (moreRecommendations == "n"):
+            keepgoing = False
+
+        else:
+            continue
         
     if userFoodChoice == lowerCaseFoodChoices[6]:
         print("Recommendation: " + foodLocations["Seafood Boil"])
+
+        # asking if user wants to keep finding restaurant recommendations
+        moreRecommendations = input("Want more recommendations [Y|N]?").lower()
+        if(moreRecommendations == "y"):
+            userFoodChoice = input("Please select your choice above and we'll recommend places to eat: ").lower()
+        elif (moreRecommendations == "n"):
+            keepgoing = False
+
+        else:
+            continue
         
     if userFoodChoice == lowerCaseFoodChoices[7]:
         print("Recommendation: " + foodLocations["Sushi"])
 
+        # asking if user wants to keep finding restaurant recommendations
+        moreRecommendations = input("Want more recommendations [Y|N]? ").lower()
+        if(moreRecommendations == "y"):
+            userFoodChoice = input("Please select your choice above and we'll recommend places to eat: ").lower()
+        elif (moreRecommendations == "n"):
+            keepgoing = False
+
+        else:
+            continue
+
     if userFoodChoice == lowerCaseFoodChoices[8]:
         print("Recommendation: " + foodLocations["Tacos"])
+
+        # asking if user wants to keep finding restaurant recommendations
+        moreRecommendations = input("Want more recommendations [Y|N]?").lower()
+        if(moreRecommendations == "y"):
+            userFoodChoice = input("Please select your choice above and we'll recommend places to eat: ").lower()
+        elif (moreRecommendations == "n"):
+            keepgoing = False
+
+        else:
+            continue
         
     if userFoodChoice == lowerCaseFoodChoices[9]:
-        print("Recommendation: " + foodLocations["American BBQ"]) 
+        print("Recommendation: " + foodLocations["American BBQ"])
 
-   # else:
-    #    print("Not a valid food choice")
+        # asking if user wants to keep finding restaurant recommendations
+        moreRecommendations = input("Want more recommendations [Y|N]?").lower()
+        if(moreRecommendations == "y"):
+            userFoodChoice = input("Please select your choice above and we'll recommend places to eat: ").lower()
+        elif (moreRecommendations == "n"):
+            keepgoing = False
+
+        else:
+            continue 
+
+   
         
 

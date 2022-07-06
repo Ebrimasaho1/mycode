783185338ccb0197167afa9e5c384faedc0f77d9
Ebrimasaho1 <saho.ebrima@ymail.com>
2022-07-06 23:00:16 +0000

    added endpoints and returns a html file

---
+++ flask_project/alta3research-flask01.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from flask import Flask
+from flask import redirect
+from flask import request
+from flask import render_template
+from flask import url_for
+app = Flask(__name__)
+
+## This is where we want to redirect users to
+@app.route("/success/<answer>") # missing a trailing >
+def success(answer):
+    return f"The answer is {answer}\n"
+
+# first endpint
+# This is a landing point for users (a start)
+@app.route("/") # user can land at "/"
+@app.route("/start") # or user can land at "/start"
+def start():
+    return render_template("index.html") 
+
+# second endpoint                            
+@app.route("/correct", methods = ["POST"])   
+def answer():
+    if request.method == "POST":
+        if request.form.get('nm'): # check if there's a value first
+            # then check if it's the value you want
+            if request.form.get('nm').lower() == 'jupiter':
+                ans = request.form.get("nm")
+                return redirect(url_for("success", answer = ans))
+        # complete the line below; if we didn't get returned to /success for getting the answer right, return the client back to / or /start to try again
+            else:
+                return redirect(url_for('/'))
+   
+
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=2224) # runs the application
+
+    
+

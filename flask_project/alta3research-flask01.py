af9ae445fc8233e6a67bd6f29e1a512c29bee2fc
Ebrimasaho1 <saho.ebrima@ymail.com>
2022-07-08 20:42:26 +0000

    added extra comments-styling-and some printing

--- flask_project/alta3research-flask01.py
+++ flask_project/alta3research-flask01.py
@@ -14,24 +14,29 @@ app = Flask(__name__)
 def success(answer):
     return f"Correct! The answer is {answer}\n"
 
-# first endpint
+# First endpint
 # This is a landing point for users (a start)
 @app.route("/") # user can land at "/"
 @app.route("/start") # or user can land at "/start"
 def start():
     return render_template("index.html") 
 
-# second endpoint                            
+# Second endpoint
+# This endpoint determines if user input is right or incorrect
+# accepts POST requests 
 @app.route("/correct", methods = ["POST"])   
 def answer():
     if request.method == "POST":
         if request.form.get('nm'): # check if there's a value first
+           
             # then check if it's the value you want
+            # Alo redirects the user to the success endpoint 
             if request.form.get('nm').lower() == 'jupiter':
                 ans = request.form.get("nm")
                 return redirect(url_for("success", answer = ans))
         # complete the line below; if we didn't get returned to /success for getting the answer right, return the client back to / or /start to try again
             else:
+                # this redirects the user to the starting point wnen if the input is incorrect
                 return redirect(url_for('start'))
    
 
@@ -67,13 +72,14 @@ gotdata = {
             ]}
 
 
-
+# Third endpoint
 # end point that returns json
 @app.route("/json", methods = ["GET"])
 def showmethejson():
     return jsonify(gotdata)
 
 
+# Fourth endpoint
 # end point to add new character in dictionary
 @app.route("/newcharacter", methods = ["POST"])
 def addChar():
@@ -86,13 +92,12 @@ def addChar():
     # adding the object into the dictionary
     gotdata['characters'].append(json_dict)
 
-                # why not return the json data instead...
-                # changed from /start to /json
+    # this redirect helps the json to be rendered with the changes when a new object is appended to the "characters" game of thrones dictionary/object 
     return redirect('/json')
 
 
 
-
+# runs the server
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=2224) # runs the application
 
--- flask_project/alta3research-requests02.py
+++ flask_project/alta3research-requests02.py
@@ -3,11 +3,15 @@ import requests
 import json
 from pprint import pprint
 
+# url that returns the json 
 URL= "http://127.0.0.1:2224/json"
 resp= requests.get(URL).json()
 
+print("Original response")
+
 pprint(resp)
 
+# new object to be added to the json/dictionary 
 newcharacter = {
              "name" : "Jon Snow",
              "house" : "House Stark",
@@ -17,5 +21,9 @@ newcharacter = {
 # posting to our endpoint
 resp_post = requests.post("http://127.0.0.1:2224/newcharacter", json=json.dumps(newcharacter))
 
+print('------------------------------------------------')
+print("New response after the new object is posted")
+
+# pretty print the response for a cleaner and more readeable output
 pprint(resp_post.json())
 
